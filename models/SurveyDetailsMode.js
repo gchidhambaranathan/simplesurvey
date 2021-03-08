c61940b8478e11220b271391cd3602ec4d9a0def
G chidhambaranathan <chidhamG@corp.ad.activcard.com>
2021-03-08 21:26:11 +0530

    Adding Survey model class

---
+++ models/SurveyDetailsMode.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+
+
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema ({
+    question : {type: String, required : ture, length : 500},
+    ans_options: {type: Array, required: true,'default': ['Strongly disagree',
+                'Somewhat disagree', 'Neither agree nor disagree', 'Somewhat agree', 'Strongly agree']}
+})
+
+
+var Survey = new Schema({
+     survey_name : {type: String, required: true, length: 100},
+     questions : [QuestionSchema]
+})
---
+++ service/RolePermissionService.js
@@ -0,0 +1,96 @@
+const RolePermission = require('../models/RolePermission')
+
+
+exports.createRolePermissions = function createRolePermissions(req, res) {
+    var permissionSets = [];
+
+    req.body.permissionSetItems.forEach(element => {
+        var permissionSet = {
+            name: element.name,
+            permissions:element.permissions
+           
+        }
+        permissionSets.push(permissionSet)
+    });
+
+    console.log(permissionSets)
+
+    var rolePermission = new RolePermission({
+        role_id : req.body.roleId,
+        permission_set_items: permissionSets
+    })
+
+    RolePermission.create(rolePermission).then(data => {
+        res.status(201).send(data)
+    }).catch(error => {
+        res.status(500).send({
+            errorMessage: error
+        })
+    })
+}
+
+exports.getPermissionByRole = function(req, res){
+    var role = req.params.roleid
+    var filter = {role_id : role}
+    console.log(filter)
+    RolePermission.find(filter, function(err, data){
+
+        if(data){
+            res.status(200).send(data)
+        }else if(err){
+            res.status(500).send({
+                errorMessage: err
+            })
+        }
+    })
+}
+
+
+exports.getAllRolePermissions = function getAllRolePermissions (req, res){
+    const filter = {}
+
+    RolePermission.find(filter, (err, data) => {
+
+        if(data){
+            res.status(200).send(data)
+        }else if(err){
+            res.status(500).send({
+                errorMessage : "Unable to fetch all role permissions"
+            })
+        }
+    })
+
+}
+
+exports.deletePermissionByRole = function deletePermissionByRole(req, res){
+    RolePermission.findByIdAndDelete(req.params.uid).then(data){
+
+        if(!data) {
+            res.status(404).send({
+                messgae : 'not found'
+            })
+        }else {
+           res.status(204).send({
+               messgae : 'Deleted'
+           }) 
+        }
+    }
+}
+
+exports.updateRolePermission = function updateRolePermission(req, res){
+    RolePermission.findByIdAndUpdate(req.params.uid, req.body, { useFindAndModify: false }).then(data => {
+        if(!data){
+            res.status(500).send({
+                messgae: "Unable to update"
+            })
+        }else {
+            res.status(200).send({
+                messgae: "Updated Successfully"
+            })
+        }
+    }).catch(err => {
+        res.status(500).send({
+        errorMessage: "Error updating Tutorial with id=" + req.params.uid + err
+        });
+    });
+}
